232acfe550415b70f5abd602f5355107436d8819
binti29 <bintiqomariah11@gmail.com>
2017-11-18 14:56:11 +0700

    mengubah fle ujian.sql

--- ujian.sql
+++ ujian.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: Nov 17, 2017 at 09:03 PM
+-- Generation Time: Nov 18, 2017 at 01:29 PM
 -- Server version: 10.0.31-MariaDB-0ubuntu0.16.04.2
 -- PHP Version: 7.0.25-1+ubuntu16.04.1+deb.sury.org+1
 
@@ -42,24 +42,6 @@ INSERT INTO `jenis_kelamin` (`id_jenis`, `jenis_kelamin`) VALUES
 -- --------------------------------------------------------
 
 --
--- Table structure for table `mapel`
---
-
-CREATE TABLE `mapel` (
-  `id_mapel` int(11) NOT NULL,
-  `id_siswa` int(11) NOT NULL,
-  `matematika` int(11) NOT NULL,
-  `b.indonesia` int(11) NOT NULL,
-  `b.inggris` int(11) NOT NULL,
-  `produktif1` int(11) NOT NULL,
-  `produktif2` int(11) NOT NULL,
-  `produktif3` int(11) NOT NULL,
-  `produktif4` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
-
--- --------------------------------------------------------
-
---
 -- Table structure for table `siswa`
 --
 
@@ -68,9 +50,22 @@ CREATE TABLE `siswa` (
   `no_ujian` int(20) NOT NULL,
   `nama_siswa` varchar(100) COLLATE utf8mb4_unicode_ci NOT NULL,
   `jurusan` varchar(50) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `jenis_kelamin` varchar(12) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `id_jk` int(11) NOT NULL
+  `id_jk` int(11) NOT NULL,
+  `matematika` int(11) NOT NULL,
+  `b_inggris` int(11) NOT NULL,
+  `b_indonesia` int(11) NOT NULL,
+  `produktif1` int(11) NOT NULL,
+  `produktif2` int(11) NOT NULL,
+  `produktif3` int(11) NOT NULL,
+  `produktif4` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `siswa`
+--
+
+INSERT INTO `siswa` (`id_siswa`, `no_ujian`, `nama_siswa`, `jurusan`, `id_jk`, `matematika`, `b_inggris`, `b_indonesia`, `produktif1`, `produktif2`, `produktif3`, `produktif4`) VALUES
+(1, 2017000629, 'Binti Qomariah', 'RPL', 2, 86, 85, 87, 75, 76, 87, 89);
 
 --
 -- Indexes for dumped tables
@@ -81,12 +76,6 @@ CREATE TABLE `siswa` (
 --
 ALTER TABLE `jenis_kelamin`
   ADD PRIMARY KEY (`id_jenis`);
-
---
--- Indexes for table `mapel`
---
-ALTER TABLE `mapel`
-  ADD PRIMARY KEY (`id_mapel`);
 
 --
 -- Indexes for table `siswa`
@@ -104,15 +93,10 @@ ALTER TABLE `siswa`
 ALTER TABLE `jenis_kelamin`
   MODIFY `id_jenis` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 --
--- AUTO_INCREMENT for table `mapel`
---
-ALTER TABLE `mapel`
-  MODIFY `id_mapel` int(11) NOT NULL AUTO_INCREMENT;
---
 -- AUTO_INCREMENT for table `siswa`
 --
 ALTER TABLE `siswa`
-  MODIFY `id_siswa` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id_siswa` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
