ba8f95bad4c79aa0d6571069a935e7707803ddb5
fahrululil <fahrululil67@gmail.com>
2017-11-24 14:53:58 +0700

    menambahkan folder include

---
+++ include/koneksi.php
@@ -0,0 +1,13 @@
+<?php
+$host = "localhost";
+$user = "root";
+$pass = "ulil";
+$dbname = "nilai";
+
+$koneksi = mysqli_connect($host, $user, $pass, $dbname);
+
+/*if($koneksi){
+echo "Sukses";
+}else{
+ echo "Koneksi gagal";
+}*/
